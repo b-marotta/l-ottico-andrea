9df8fa15a0a37b1d5b180c784df2c7e1bb6238b1
Benedetto Marotta <benedettomarotta@hotmail.it>
2025-02-11 18:36:42 +0100

    Init prettier & eslint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import eslint from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	eslint.configs.recommended,
+	prettier,
+	{
+		plugins: {
+			prettier: pluginPrettier,
+		},
+		rules: {
+			'prettier/prettier': ['error', { semi: false }], // Disabilita i punti e virgola
+		},
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,jsx}'],
+	},
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	pluginJs.configs.recommended,
+	pluginReact.configs.flat.recommended,
+]
