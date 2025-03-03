6b4ef09ebd044591fb96bf2b4705a4db2fd29750
Benedetto Marotta <benedettomarotta@hotmail.it>
2025-03-03 18:21:36 +0100

    New section: misurazioni strumentali

--- src/pages/servizi.jsx
+++ src/pages/servizi.jsx
@@ -1,85 +1,166 @@
-import { Box, Text, Separator, Image } from '@chakra-ui/react'
+import { Box, Text, Separator, Image, Stack } from '@chakra-ui/react'
 import React from 'react'
 import image_src from '../assets/photos/02.jpg'
 import styles from '../theme/pages/servizi'
 import lac_src from '../assets/other/lac.jpg'
 import lac_2_src from '../assets/other/lac_2.png'
+import pachimetria from '../assets/other/pachimetria.jpg'
+import opacity from '../assets/other/opacity.webp'
+import vx120 from '../assets/other/VX120.webp'
+import lacrimale from '../assets/other/lacrimale.webp'
 
 const Servizi = () => {
 	return (
 		<Box>
-			<Box>
-				<Box position={'relative'} bgImage={`url(${image_src})`} {...styles.main_image}>
-					<Box {...styles.overlayText}>
-						<Text textStyle={'6xl'} fontWeight={600} color={'white'}>
-							I NOSTRI SERVIZI
-						</Text>
-					</Box>
+			<Box position={'relative'} bgImage={`url(${image_src})`} {...styles.main_image}>
+				<Box {...styles.overlayText}>
+					<Text textStyle={'6xl'} fontWeight={600} color={'white'}>
+						I NOSTRI SERVIZI
+					</Text>
 				</Box>
-				<Box py={16} px={6}>
-					<Box textAlign={'center'}>
-						<Text textStyle={'3xl'} fontWeight={600} color={'primary'}>
-							Analisi visiva optometrica
-						</Text>
-						<Text textStyle={'lg'} fontWeight={500} color={'gray.500'} mb={6}>
-							Il cuore dei nostri servizi
-						</Text>
-						<Text textStyle={'xl'} fontWeight={500} color={'gray.600'} w={'60%'} mx={'auto'}>
-							Un processo accurato che utilizza tecniche di rifrazione ottica per determinare con
-							precisione le tue diottrie. Attraverso l&apos;uso di strumenti all&apos;avanguardia,
-							identifichiamo le lenti ideali per migliorare la tua vista, offrendoti soluzioni su
-							misura per soddisfare le tue esigenze.
-						</Text>
-					</Box>
+			</Box>
+			<Box py={16} px={6}>
+				<Box textAlign={'center'}>
+					<Text textStyle={'3xl'} fontWeight={600} color={'primary'}>
+						Analisi visiva optometrica
+					</Text>
+					<Text textStyle={'lg'} fontWeight={400} color={'gray.400'} mb={6}>
+						Il cuore dei nostri servizi
+					</Text>
+					<Text textStyle={'xl'} fontWeight={400} color={'gray.600'} w={'60%'} mx={'auto'}>
+						Un processo accurato che utilizza tecniche di rifrazione ottica per determinare con
+						precisione le tue diottrie. Attraverso l&apos;uso di strumenti all&apos;avanguardia,
+						identifichiamo le lenti ideali per migliorare la tua vista, offrendoti soluzioni su
+						misura per soddisfare le tue esigenze.
+					</Text>
 				</Box>
-				<Separator />
-				<Box py={16} px={6}>
-					<Box textAlign={'center'} pb={16}>
+			</Box>
+			<Separator />
+			<Box py={16} px={6}>
+				<Box textAlign={'center'} pb={16}>
+					<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
+						Applicazione lenti a contatto
+					</Text>
+					<Text textStyle={'xl'} fontWeight={400} color={'gray.600'} w={'60%'} mx={'auto'}>
+						Nel nostro centro ottico, offriamo una vasta gamma di soluzioni per le lenti a contatto,
+						adattate alle esigenze specifiche di ogni cliente.
+					</Text>
+				</Box>
+				<Box
+					w={'60%'}
+					mx={'auto'}
+					display={'grid'}
+					gridTemplateColumns={'1fr 1fr'}
+					alignItems={'center'}
+					gap={12}
+				>
+					<Box>
+						<Image src={lac_src} borderRadius={16} />
+					</Box>
+					<Box>
 						<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
-							Applicazione lenti a contatto
+							LAC morbide
 						</Text>
-						<Text textStyle={'xl'} fontWeight={500} color={'gray.600'} w={'60%'} mx={'auto'}>
-							Nel nostro centro ottico, offriamo una vasta gamma di soluzioni per le lenti a
-							contatto, adattate alle esigenze specifiche di ogni cliente.
+						<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+							Proponiamo l&apos;applicazione di lenti a contatto morbide, sia su misura che
+							standard, per garantire il massimo comfort e una visione nitida. Queste lenti sono
+							ideali per chi cerca una soluzione confortevole e facile da utilizzare nella vita
+							quotidiana.
 						</Text>
 					</Box>
-					<Box
-						w={'60%'}
-						mx={'auto'}
-						display={'grid'}
-						gridTemplateColumns={'1fr 1fr'}
-						alignItems={'center'}
-						gap={12}
-					>
-						<Box>
-							<Image src={lac_src} borderRadius={16} />
-						</Box>
-						<Box>
-							<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
-								LAC morbide
+					<Box textAlign={'right'}>
+						<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
+							LAC RGP
+						</Text>
+						<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+							Siamo specializzati nell&apos;applicazione di lenti a contatto rigide gas permeabili
+							(RGP), ideali per chi necessita di una correzione visiva più stabile e duratura. Per
+							chi presenta irregolarità corneali, offriamo lenti a contatto RGP su misura,
+							progettate per adattarsi perfettamente alla superficie oculare e migliorare
+							significativamente la qualità visiva.
+						</Text>
+					</Box>
+					<Box bg={'gray.100'} borderRadius={16}>
+						<Image src={lac_2_src} mixBlendMode={'multiply'} my={6} />
+					</Box>
+				</Box>
+			</Box>
+			<Separator />
+			<Box py={16} px={6}>
+				<Box textAlign={'center'} pb={16}>
+					<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
+						Misurazioni strumentali
+					</Text>
+					<Text textStyle={'xl'} fontWeight={400} color={'gray.600'} w={'60%'} mx={'auto'}>
+						Utilizziamo tecnologie avanzate per effettuare misurazioni strumentali precise e
+						affidabili, garantendo una valutazione completa della salute visiva di ogni cliente.
+					</Text>
+				</Box>
+
+				<Box
+					w={'70%'}
+					mx={'auto'}
+					gapX={24}
+					gapY={12}
+					textAlign={'center'}
+					display={'grid'}
+					gridTemplateColumns={'1fr 1fr'}
+					flexWrap={'wrap'}
+				>
+					<Box>
+						<Stack gap={4}>
+							<Text textStyle={'xl'} fontWeight={600} color={'primary'}>
+								Topografia Pachimetrica
 							</Text>
-							<Text textStyle={'lg'} fontWeight={500} color={'gray.600'}>
-								Proponiamo l&apos;applicazione di lenti a contatto morbide, sia su misura che
-								standard, per garantire il massimo comfort e una visione nitida. Queste lenti sono
-								ideali per chi cerca una soluzione confortevole e facile da utilizzare nella vita
-								quotidiana.
+							<Image src={pachimetria} w="100%" h="300px" borderRadius={6} />
+							<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+								Esame non invasivo, volto a valutare la forma e lo spessore della cornea,
+								assicurando una correzione visiva ottimale e personalizzata.
 							</Text>
-						</Box>
-						<Box textAlign={'right'}>
-							<Text textStyle={'3xl'} fontWeight={600} color={'primary'} mb={6}>
-								LAC RGP
+						</Stack>
+					</Box>
+					<Box>
+						<Stack gap={4}>
+							<Text textStyle={'xl'} fontWeight={600} color={'primary'}>
+								Analisi dell&apos;opacità
 							</Text>
-							<Text textStyle={'lg'} fontWeight={500} color={'gray.600'}>
-								Siamo specializzati nell&apos;applicazione di lenti a contatto rigide gas permeabili
-								(RGP), ideali per chi necessita di una correzione visiva più stabile e duratura. Per
-								chi presenta irregolarità corneali, offriamo lenti a contatto RGP su misura,
-								progettate per adattarsi perfettamente alla superficie oculare e migliorare
-								significativamente la qualità visiva.
+							<Image src={opacity} w="100%" h="300px" borderRadius={6} />
+							<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+								Rilevazione di eventuali alterazioni del cristallino tramite retro-illuminazione,
+								contribuisce a mantenere alta la salute oculare complessiva.
 							</Text>
-						</Box>
-						<Box bg={'gray.100'} borderRadius={16}>
-							<Image src={lac_2_src} mixBlendMode={'multiply'} my={6} />
-						</Box>
+						</Stack>
+					</Box>
+					<Box>
+						<Stack gap={4}>
+							<Text textStyle={'xl'} fontWeight={600} color={'primary'}>
+								Tonometria a soffio
+							</Text>
+							<Image
+								src={vx120}
+								bg={'#e5e5e5'}
+								objectFit="contain"
+								w="100%"
+								h="300px"
+								borderRadius={6}
+							/>
+							<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+								Metodo non invasivo per misurare la pressione intraoculare (IOP), fornisce
+								un&apos;importante indicazione sullo stato di salute degli occhi.
+							</Text>
+						</Stack>
+					</Box>
+					<Box>
+						<Stack gap={4}>
+							<Text textStyle={'xl'} fontWeight={600} color={'primary'}>
+								Indagine lacrimale
+							</Text>
+							<Image src={lacrimale} w="100%" h="300px" borderRadius={6} />
+							<Text textStyle={'lg'} fontWeight={400} color={'gray.600'}>
+								L&apos;indagine lacrimale valuta quantità e qualità del film lacrimale, aiuta a
+								prevenire problemi di secchezza oculare e a garantire il comfort visivo.
+							</Text>
+						</Stack>
 					</Box>
 				</Box>
 			</Box>
